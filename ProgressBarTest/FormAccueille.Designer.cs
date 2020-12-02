787dee50ad166521a848e3d4a7b325224983b3cd
cedrdjio <69305726+cedrdjio@users.noreply.github.com>
2020-12-02 02:36:31 +0100

    add nouveau formulaire

--- ProgressBarTest/FormAccueille.Designer.cs
+++ ProgressBarTest/FormAccueille.Designer.cs
@@ -29,13 +29,29 @@ namespace ProgressBarTest
         /// </summary>
         private void InitializeComponent()
         {
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
             this.SuspendLayout();
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(232, 126);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(180, 45);
+            this.guna2Button1.TabIndex = 0;
+            this.guna2Button1.Text = "guna2Button1";
             // 
             // FormAccueille
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(515, 450);
+            this.Controls.Add(this.guna2Button1);
             this.Name = "FormAccueille";
             this.Text = "FormAccueille";
             this.ResumeLayout(false);
@@ -43,5 +59,7 @@ namespace ProgressBarTest
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
     }
 }
